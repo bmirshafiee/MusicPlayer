caf0f12ba54bf5a5e6964d2e27d2cb57f5fa80d5
Bita Mirshafiee <bitamirshafiee@Bitas-MacBook-Pro.local>
2019-01-12 16:26:50 +0330

    first commit

---
+++ app/src/main/java/com/example/bitamirshafiee/musicplayer/ItemClicked.kt
@@ -0,0 +1,6 @@
+package com.example.bitamirshafiee.musicplayer
+
+interface ItemClicked {
+
+    fun itemClicked(position : Int)
+}
---
+++ app/src/main/java/com/example/bitamirshafiee/musicplayer/Music.kt
@@ -0,0 +1,3 @@
+package com.example.bitamirshafiee.musicplayer
+
+data class Music(var artistName:String, var songName : String, var songUri : String)
